a85627194bfb844ef0db175870bf9c2a23f12cd9
David Gao <dgao@redhat.com>
2014-04-17 22:56:04 -0400

    Changed mongodb schema to be one degree more sophesticated. Added client and username collection.

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,6 +1,7 @@
 from pymongo import Connection
 
 import base64
+import copy
 import sys
 
 TIME = []
@@ -14,32 +15,71 @@ for x in range(9,19):
 INITIAL_TIME_STATE = ['CLOSED'] * 20
 INITIAL_SCHEDULE=dict(zip(TIME, INITIAL_TIME_STATE))
 
-BUSINESS = [(1, 'Hair Studio 919', 'styling919'), (2, 'Mike\'s Auto Shop', 'mikeshop')]
-SCHEDULE = {1: INITIAL_SCHEDULE, 
-            2: INITIAL_SCHEDULE
+BUSINESSES = [(1, 'Hair Studio 919', 'styling919'), (2, 'Mike\'s Auto Shop', 'mikeshop')]
+CLIENTS = [(1, 'Alex', 'alex'), (2, 'Dave', 'dave'), (3, 'Jay', 'jay'), (4, 'Jenna', 'jenna'), (5, 'Leif', 'leif'), (6, 'Matt', 'matt')]
+SCHEDULE = {1: copy.deepcopy(INITIAL_SCHEDULE), 
+            2: copy.deepcopy(INITIAL_SCHEDULE)
            }
+
+# Hardcode schedule opening for business #1
+SCHEDULE[1]['09:00'] = "OPEN"
+SCHEDULE[1]['11:30'] = "OPEN"
+SCHEDULE[1]['16:30'] = "OPEN"
 
 if __name__ == "__main__":
 	conn = Connection()
 	db = conn['procrastinate']
 	
-    # Create business w/ schedules
+    # Create business
 	collection = db['business']
+	collection.ensure_index('id', unique=True)
+    
+    # Build document
+	for id, name, username in BUSINESSES:
+		doc = ({"id": id, 
+                "name": name
+              })
+		collection.insert(doc)
+
+	# Create client
+	collection = db['clients']
+	collection.ensure_index('id', unique=True)
+    
+    # Build document
+	for id, name, username in CLIENTS:
+		doc = ({"id": id, 
+                "name": name
+              })
+		collection.insert(doc)
+
+	# Create users
+	collection = db['users']
 	collection.ensure_index('username', unique=True)
     
     # Build document
-	for id, name, username in BUSINESS:
+	for id, name, username in BUSINESSES:
+		doc = ({"id": id, 
+                "username": username,
+                "password": base64.b64encode("password")
+              })
+		collection.insert(doc)
+	for id, name, username in CLIENTS:
+		doc = ({"id": id, 
+                "username": username,
+                "password": base64.b64encode(username)
+              })
+		collection.insert(doc)
+
+	# Create schedule
+	collection = db['schedule']
+	collection.ensure_index('id', unique=True)
+    
+    # Build document
+	for id, name, username in BUSINESSES:
 		schedule = SCHEDULE[id]
-		if id == 2: # for business #2, open up two appointments
-			schedule['09:00'] = "OPEN"
-			schedule['11:30'] = "OPEN"
-			schedule['16:30'] = "OPEN"
-			
+		print id, schedule
 
 		doc = ({"id": id, 
-                "name": name,
-                "schedule": schedule,
-				"username": username,
-				"password": base64.b64encode("password")
+                "schedule": schedule
               })
 		collection.insert(doc)
