4f36f1205f66212234ee1d0c22aedfa0543b78e3
David Gao <dgao@redhat.com>
2014-04-18 00:32:16 -0400

    Updated API for login. Created API for fetching schedule of businesses.

--- src/server/app.py
+++ src/server/app.py
@@ -14,19 +14,46 @@ db = conn['procrastinate']
 def login():
 	if not request.args or not 'username' in request.args or not 'password' in request.args:
 		abort(400)
-	collection = db['business']
+
+	collection = db['users']
 	try:
 		user = list(collection.find({"username": request.args['username']}, {"_id": 0})).pop()
 	except IndexError:
+		print "Cannot find entry in database with username: %s." % request.args['username']
 		abort(401)	
 
 	if not user['password'] == request.args['password']:
 		abort(401)
 
-	rtn = copy.deepcopy(user)
-	# pop off password field
-	del rtn['password']
-	return json.dumps(rtn), 201
+	id = user['id']
+	if user['type'] == "business":
+		collection = db['business']
+	else:
+		collection = db['clients']
+
+	rtn = list(collection.find({"id": id}, {"_id": 0})).pop() # There's a possibility that pop() will fail if DB is not lined up correctly. In this scenario, wtf...?
+	rtn['type'] = user['type'] # Add this back on so UI knows which pane to paint
+	return json.dumps(rtn), 200
+
+@app.route('/api/v1.0/schedule', methods = ['GET'])
+def get_schedule():
+	if not request.args or not 'id' in request.args:
+		abort(400)
+
+	collection = db['schedule']
+	try:
+		schedule = list(collection.find({"id": int(request.args['id'])}, {"_id": 0})).pop()
+	except IndexError:
+		# WTF? How can this happen?
+		print "Cannot find entry in database with id: %s." % request.args['id']
+		abort(400) 
+	except ValueError:
+		print "Converting request field to int failed."
+		abort(400)
+	except Exception as e:
+		print "Unknown exception:", e
+		abort(400)
+	return json.dumps(schedule), 200
 
 @app.route('/api/v1.0/business', methods = ['GET'])
 def get_biz():
